e988a4b00fdd343040771d8cf389d72f0b9145b1
PavelMekhnin <mps_kz@mail.ru>
2020-02-23 11:23:37 +0300

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Animal shelter</h1>
+      <p>Everybody may have a famaly :3</p>
+    </div>
+  );
+}
+
+export default App;
